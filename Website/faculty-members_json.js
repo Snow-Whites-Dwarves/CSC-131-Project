5ed47688f53a958b620696379cd129dd706286dd
Chris West <chriswest4@gmail.com>
2017-03-10 08:41:03 -0800

    Added Website files

---
+++ Website/faculty-members_json.js
@@ -0,0 +1,132 @@
+{
+	"person": [
+		{
+			"name": {
+				"first": "Pinar",
+				"last": "Muyan-Ozcelik",
+				"suffix": "",
+				"title": "Dr"
+			},
+			"office": {
+				"building": "Riverside",
+				"room": 5008,
+				"hours": [
+					{
+						"day": "Tuseday",
+						"hour": "3:00PM-3:50PM"
+					},
+					{
+						"day": "Thursday",
+						"hour": "1:40PM-3:50PM"
+					}
+				]
+			},
+			"contact": {
+				"phone": "916-278-6713",
+				"email": "",
+				"website": ""
+			},
+			"courses": [
+				{
+					"course": "CSC 133",
+					"section": 1,
+					"days": "TR",
+					"time": "12:00PM-1:15PM",
+					"room": "ARC 3004"
+				},
+				{
+					"course": "CSC 133",
+					"section": 3,
+					"days": "TR",
+					"time": "4:00PM-5:15PM",
+					"room": "ARC 3004"
+				},
+				{
+					"course": "CSC 196U",
+					"section": 1,
+					"days": "TR",
+					"time": "10:30AM-11:45AM",
+					"room": "RVR 5029"
+				}
+			]
+		},
+		{
+			"name": {
+				"first": "Chung-E",
+				"last": "Wang",
+				"suffix": "",
+				"title": "Dr."
+			},
+			"office": {
+				"building": "Riverside",
+				"room": "5056",
+				"hours": [
+					{
+						"day": "Monday/Wednesday",
+						"hour": "4:40PM-5:20PM"
+					},
+					{
+						"day": "Tuesday",
+						"hour": "10:40AM-11:20AM"
+					}
+				]
+			},
+			"contact": {
+				"phone": "",
+				"email": "wang@ecs.csus.edu",
+				"website": ""
+			},
+			"courses": [
+				{
+					"course": "CSC 20",
+					"section": 1,
+					"days": "MW",
+					"time": "2:00PM-2:50PM",
+					"room": "RVR 1008"
+				},
+				{
+					"course": "CSC 20",
+					"section": 2,
+					"days": "M",
+					"time": "3:00PM-4:40PM",
+					"room": "RVR 2013"
+				},
+				{
+					"course": "CSC 20",
+					"section": 3,
+					"days": "W",
+					"time": "3:00PM-4:40PM",
+					"room": "RVR 2013"
+				},
+				{
+					"course": "CSC 20",
+					"section": 4,
+					"days": "TR",
+					"time": "8:00AM-8:50AM",
+					"room": "RVR 1002"
+				},
+				{
+					"course": "CSC 20",
+					"section": 5,
+					"days": "T",
+					"time": "9:00AM-10:40AM",
+					"room": "RVR 2013"
+				},
+				{
+					"course": "CSC 20",
+					"section": 6,
+					"days": "R",
+					"time": "9:00AM-10:40AM",
+					"room": "RVR 2013"
+				},
+				{
+					"course": "CSC 206",
+					"section": 1,
+					"days": "MW",
+					"time": "5:30PM-6:45PM",
+					"room": "RVR 1008"
+				}
+			]
+		}
+	]
+}
